f5378efcead69d73abe9ffa57fe759168f346960
jg932 <jared.h.goldstein@gmail.com>
2021-11-18 20:51:32 -0500

    double check

--- clues.sql
+++ clues.sql
@@ -20,7 +20,6 @@ SELECT language FROM countrylanguage WHERE countrycode = 'VAT';
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 -- TODO: Write SQL query here
 SELECT * FROM countrylanguage WHERE language = 'Italian' AND percentage = '100';
-SELECT * FROM country WHERE code = 'SMR';
 
 
 
